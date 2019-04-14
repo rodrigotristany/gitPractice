16195d556ef70f20c2329a59345108764d6a8d1a
Rodrigo Tristany <rodrigo.tristany@goempirical.com>
2019-04-14 17:06:16 -0300

    Primer push

---
+++ gitPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
